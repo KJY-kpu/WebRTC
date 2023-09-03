7d0d1e28f006d2057e5e4e08bc399384a1010892
KJY <rntlls1366@gmail.com>
2023-09-04 00:35:10 +0900

    WebRTC 활용 영상통화 구현

---
+++ zoomRTC/src/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import http from "http";
+import {WebSocketServer} from "ws";
+import SocketIO from "socket.io";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.engine("pug", require("pug").__express);
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListen = () => console.log('Listening on http://localhost:3000');
+
+const httpServer = http.createServer(app);
+
+const wsServer = SocketIO(httpServer);
+
+wsServer.on("connection", socket => {
+  socket.on("join_room", (roomName) => {
+    socket.join(roomName);
+    socket.to(roomName).emit("welcome");
+  });
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer);
+  });
+  socket.on("answer", (answer, roomName) => {
+    socket.to(roomName).emit("answer", answer);
+  });
+  socket.on("ice", (ice, roomName) => {
+    socket.to(roomName).emit("ice", ice);
+  })
+})
+
+httpServer.listen(3000, handleListen);
