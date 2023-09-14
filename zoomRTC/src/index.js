f3a7422debf67fab446dfbf32c7f602c56638c81
KJY-kpu <rntlls1366@gmail.com>
2023-09-14 17:45:34 +0900

    데모서버연동중

--- zoomRTC/src/index.js
+++ zoomRTC/src/index.js
@@ -1,263 +1,15 @@
-import express from "express";
-import http from "http";
-import { WebSocketServer } from "ws";
-import SocketIO from "socket.io";
-import e from "express";
-import { start } from "repl";
-import {
-  Device,
-  RTCRtpCodecParameters,
-  useAbsSendTime,
-  useFIR,
-  useNACK,
-  usePLI,
-  useREMB,
-  useSdesMid,
-  MediaStreamTrack,
-} from "msc-node";
-
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
-
-const WebSocket = require('ws');
-
-const fs = require('fs');
-const path = require('path');
-const wrtc = require("wrtc");
-
-
-const app = express();
-const httpServer = http.createServer(app);
-const wsServer = SocketIO(httpServer);
-
-app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
-app.engine("pug", require("pug").__express);
-
-app.use("/public", express.static(__dirname + "/public"));
-
-app.get("/", (req, res) => res.render("home_teacher"));
-app.get("/student", (req, res) => res.render("home_student"));
-
-const handleListen = () => console.log('Listening on http://localhost:3005');
-
-let writeStream;
-
-let teacherPc;
-let teacherStream;
-let teacherSenders = {};
-let studentPc = new Map();    //소켓, peerConnection 쌍
-let reConnection = 0;
-let teacherDevice;
-let room;
-
-//---선생 RTCPeerConnection 정의
-
-const createTeacherPc = (teacherSocket) => {
-  const pc = new wrtc.RTCPeerConnection({
-    iceServers: [
-      {
-        urls: [
-          "stun:stun.l.google.com:19302",
-          "stun:stun1.l.google.com:19302",
-          "stun:stun2.l.google.com:19302",
-          "stun:stun3.l.google.com:19302",
-          "stun:stun4.l.google.com:19302",
-        ]
-      }
-    ]
-  });
-
-  pc.onicecandidate = (e) => {
-    // console.log("I sent ice");
-    teacherSocket.emit("ice", e.candidate);
-  };
-
-  pc.oniceconnectionstatechange = (e) => {
-    //console.log(e);
-  };
-
-  pc.ontrack = (e) => {
-    console.log("input stream in ts ", e.streams[0]);
-    teacherStream = e.streams[0];
-    teacherSenders = pc.getSenders();
-  };
-
-  pc.onconnectionstatechange = (e) => {
-    console.log("tpc has changed", pc.connectionState);
-    switch (pc.connectionState) {
-      case "disconnected":
-        if (teacherPc) teacherPc.close();
-        teacherStream = null;
-        teacherPc = null;
-        reConnection = 1;
-        break;
-      case "failed":
-        if (teacherPc) teacherPc.close();
-        teacherStream = null;
-        teacherPc = null;
-        reConnection = 1;
-        break;
-      case "connected":
-
-        socket = new WebSocket(`wss://localhost:3000`);
-
-        socketEvent();
-
-        if (reConnection === 1) {
-          console.log("reconnecting");
-
-          studentPc.forEach(spc => {
-            spc.close();
-          });
-
-          for (let sSock of studentPc.keys()) {
-            sSock.emit("reconnect");
-          }
-          reConnection = 0;
-        }
-
-        console.log("sent welcome to students");
-
-        for (let sSock of studentPc.keys()) {
-          sSock.emit("welcome");
-        }
-
-        break;
-
-    }
-  }
-
-  return pc;
-
-}
-
-//---학생 RTCPeerConnection 정의
-
-const createStudentPc = (studentSocket) => {
-  const pc = new wrtc.RTCPeerConnection({
-    iceServers: [
-      {
-        urls: [
-          "stun:stun.l.google.com:19302",
-          "stun:stun1.l.google.com:19302",
-          "stun:stun2.l.google.com:19302",
-          "stun:stun3.l.google.com:19302",
-          "stun:stun4.l.google.com:19302",
-        ]
-      }
-    ]
-  });
-
-  pc.onicecandidate = (e) => {
-    // console.log("I sent ice");
-    studentSocket.emit("ice", e.candidate);
-  };
-
-  pc.oniceconnectionstatechange = (e) => {
-    //console.log(e);
-  };
-
-  if (teacherStream) teacherStream.getTracks().forEach(track => pc.addTrack(track, teacherStream));
-
-  return pc;
-}
-
-//---소켓 통신
-
-wsServer.on("connection", socket => {
-
-  socket.on("test", () => {
-    console.log("test on!");
-    socket.emit("test");
-  })
-
-  socket.on("join_room", async (roomName) => {
-    room = roomName;
-    socket.join(roomName);
-    socket.emit("welcome");
-  });
-
-  socket.on('join_roomstudent', async (roomName) => {
-    room = roomName;
-    socket.join(roomName);
-    studentPc.set(socket, null);
-    if (teacherStream) socket.emit("welcome");
-  });
-
-  socket.on("offerteacher", async (offer) => {
-    console.log("start offerteacher");
-    try {
-      teacherPc = createTeacherPc(socket);
-      console.log("created pc");
-    } catch (e) { console.log(e); }
-    teacherPc.setRemoteDescription(offer);
-    console.log("set remotedDescription");
-    const answer = await teacherPc.createAnswer({
-      offerToReceiveAudio: true,
-      offerToReceivevideo: true,
-    });
-    console.log("created answer");
-
-    teacherPc.setLocalDescription(answer);
-    console.log("set localDescription");
-    socket.emit("answer", answer);
-
-  });
-
-
-  socket.on("offerstudent", async () => {
-    try {
-      studentPc.set(socket, createStudentPc(socket));
-    } catch (e) { console.log(e); }
-    const offer = await studentPc.get(socket).createOffer();
-    studentPc.get(socket).setLocalDescription(offer);
-    socket.emit("offer", offer);
-    console.log("send offer to student");
-  });
-
-  socket.on("answerstudent", (answer) => {
-    studentPc.get(socket).setRemoteDescription(answer);
-    console.log("i got answer from studnet");
-  });
-
-  socket.on("ice", (ice, role) => {
-    if (role === 0) {
-      if (ice) {
-        teacherPc.addIceCandidate(
-          new wrtc.RTCIceCandidate(ice)
-        );
-        // console.log("i got ice");
-      }
-      else {
-        // console.log("i got null ice", ice);
-      }
-
-    }
-    else {
-      if (ice) studentPc.get(socket).addIceCandidate(
-        new wrtc.RTCIceCandidate(ice)
-      );
-      // console.log("i got ice");
-    }
-  });
-})
-
-httpServer.listen(3005, handleListen);
-
-//----------------------live streaming save
-
 const mediasoup = require('mediasoup-client');
 
 const { GUM } = require('./gum');
 const Peer = require('./peer');
 const SocketQueue = require('./queue');
 
-const mediasoupConfig = require('./config');
+const mediasoupConfig = require('../../server/src/config');
 
 let peer;
 const queue = new SocketQueue();
 
-let socket;
+const socket = new WebSocket(`wss://${window.location.hostname}:3000`);
 
 const handleSocketOpen = async () => {
   console.log('handleSocketOpen()');
@@ -274,8 +26,8 @@ const handleSocketMessage = async (message) => {
 
 const handleSocketClose = () => {
   console.log('handleSocketClose()');
-  // document.getElementById('startRecordButton').disabled = true;
-  // document.getElementById('stopRecordButton').disabled = true;
+  document.getElementById('startRecordButton').disabled = true;
+  document.getElementById('stopRecordButton').disabled = true;
 };
 
 const getVideoCodecs = () => {
@@ -283,7 +35,7 @@ const getVideoCodecs = () => {
   const videoCodec = params.get('videocodec')
   console.warn('videoCodec');
 
-  const codec = mediasoupConfig.router.mediaCodecs.find(c => {
+  const codec = mediasoupConfig.router.mediaCodecs.find(c=>{
     if (!videoCodec)
       return undefined;
 
@@ -330,26 +82,11 @@ const handleRouterRtpCapabilitiesRequest = async (jsonMessage) => {
   console.log('handleRouterRtpCapabilities() [rtpCapabilities:%o]', routerRtpCapabilities);
 
   try {
-    // const device = new mediasoup.Device();
-    const device = new Device({
-      headerExtensions: {
-        video: [useSdesMid(), useAbsSendTime()],
-      },
-      codecs: {
-        video: [
-          new RTCRtpCodecParameters({
-            mimeType: "video/VP8",
-            clockRate: 90000,
-            payloadType: 98,
-            rtcpFeedback: [useFIR(), useNACK(), usePLI(), useREMB()],
-          }),
-        ],
-      },
-    });
+    const device = new mediasoup.Device();
     // Load the mediasoup device with the router rtp capabilities gotten from the server
     await device.load({ routerRtpCapabilities });
 
-    peer = new Peer(sessionId, device, teacherStream);
+    peer = new Peer(sessionId, device);
     createTransport();
   } catch (error) {
     console.error('handleRouterRtpCapabilities() failed to init device [error:%o]', error);
@@ -398,13 +135,13 @@ const handleSendTransportListeners = () => {
 };
 
 const getMediaStream = async () => {
-  const mediaStream = teacherStream;
-  
+  const mediaStream = await GUM();
+  const videoNode = document.getElementById('localVideo');
+  videoNode.srcObject = mediaStream;
+
   // Get the video and audio tracks from the media stream
   const videoTrack = mediaStream.getVideoTracks()[0];
-  console.log("비디오트랙 : ", videoTrack);
   const audioTrack = mediaStream.getAudioTracks()[0];
-  console.log("오디오트랙 : ", audioTrack);
 
   // If there is a video track start sending it to the server
   if (videoTrack) {
@@ -499,12 +236,10 @@ const handleTransportProduceEvent = ({ kind, rtpParameters }, callback, errback)
   }
 };
 
-function socketEvent() {
-  socket.addEventListener('open', handleSocketOpen);
-  socket.addEventListener('message', handleSocketMessage);
-  socket.addEventListener('error', handleSocketError);
-  socket.addEventListener('close', handleSocketClose);
-}
+socket.addEventListener('open', handleSocketOpen);
+socket.addEventListener('message', handleSocketMessage);
+socket.addEventListener('error', handleSocketError);
+socket.addEventListener('close', handleSocketClose);
 
 module.exports.startRecord = () => {
   console.log('startRecord()');
